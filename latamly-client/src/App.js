22c679a62d94e428c2c0308a4bf7d17c873ccfcc
Mariano Jiménez <marianojimenezperez1@gmail.com>
2023-04-12 11:29:44 -0300

    se estructuran carpetas del proyecto

--- latamly-client/src/App.js
+++ latamly-client/src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import CompShowImgs from './imgs/ShowImgs.js';
-import Subir from './imgs/Subir.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ContentTest from './componentes/ContentTest.js';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<ContentTest/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
+          {/* <Route path='/' element={<ContentTest/>}></Route>
           <Route path='/admin/media' element={<CompShowImgs/>}></Route>
-          <Route path='/admin/media/upload' element={<Subir/>}></Route>
+          <Route path='/admin/media/upload' element={<Subir/>}></Route> */}
         </Routes>
       </BrowserRouter>
     
