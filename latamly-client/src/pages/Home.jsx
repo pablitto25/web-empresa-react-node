51a4f6d7e022ecd1fe93341d0ffb6a5780a82184
marianojimenezperez <marianojimenezperez@gmail.com>
2023-04-13 14:58:41 -0300

    Se comienza a trabajar sobre el componente whatWeDo. Todo: responsive del componente

--- latamly-client/src/pages/Home.jsx
+++ latamly-client/src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import AddValue from '../components/AddValue/AddValue'
-import Header from "../components/Header/Header"
-import Hero from "../components/Hero/Hero"
+import React from "react";
+import AddValue from "../components/AddValue/AddValue";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import WhatWeDo from "../components/WhatWeDo/WhatWeDo";
 
 const Home = () => {
   return (
     <>
-        <Header />
-        <Hero />
-        <AddValue />
+      <Header />
+      <Hero />
+      <AddValue />
+      <WhatWeDo />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
