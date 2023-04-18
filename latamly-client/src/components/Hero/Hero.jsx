c743d28fd034727d89cc64f0887dfd41548358f2
Pablo Cortes <47508769+pablitto25@users.noreply.github.com>
2023-04-18 13:08:37 -0300

    hero arreglado, y quedo responsive

--- latamly-client/src/components/Hero/Hero.jsx
+++ latamly-client/src/components/Hero/Hero.jsx
@@ -11,17 +11,15 @@ const Hero = () => {
     <Container fluid className='justify-content-md-center' id={styles.megaContenedor} style={{ padding: 0 }}>
       <div className={styles.contenido}>
         <div className={styles.embed_responsive_16by9} >
-          <iframe src="https://www.youtube.com/embed/gy4E8MWT61s?controls=0&autoplay=1&mute=1&showinfo=0&modestbranding=1"></iframe>
+          <iframe src="https://www.youtube.com/embed/gy4E8MWT61s?controls=0&autoplay=1&loop=1&mute=1&showinfo=0&modestbranding=1"></iframe>
         </div>
         <div className={styles.overlay}>
-        <p className={styles.text1}><strong>involve to evolve</strong></p>
-        <p className={styles.text2}>We expand your business in <strong>LATIN AMERICA</strong></p>
-      </div>
+          <p className={styles.text1}><strong>involve to evolve</strong></p>
+          <p className={styles.text2}>We expand your business in <strong>LATIN AMERICA</strong></p>
+        </div>
       </div>
       
     </Container>
-
-    
 
 
   )
